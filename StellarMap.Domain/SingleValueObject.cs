23ab747486d76bada5d5b7fac40681627b119c95
Nicole Turner <astartechie@gmail.com>
2024-03-26 09:29:03 +0000

    Created Domain with Base Classes

---
+++ StellarMap.Domain/SingleValueObject.cs
@@ -0,0 +1,16 @@
+﻿namespace StellarMap.Domain;
+
+public abstract class SingleValueObject<T, TValue> : ValueObject<T> where T : ValueObject<T>
+{
+    protected SingleValueObject(TValue value)
+    {
+        Value = value;
+    }
+
+    public TValue Value { get; }
+
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
---
+++ StellarMap.Domain/ValueObject.cs
@@ -0,0 +1,24 @@
+﻿namespace StellarMap.Domain;
+
+public abstract class ValueObject<T> : IEquatable<T> where T : ValueObject<T>, IEquatable<T>
+{
+    public bool Equals(T? other)
+        => other is not null && GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+
+    public override bool Equals(object? obj)
+        => Equals(obj as T);
+
+    public override int GetHashCode()
+        => GetEqualityComponents().Select(x => x?.GetHashCode() ?? 0).Aggregate((x, y) => x ^ y);
+
+    public override string ToString()
+        => string.Join(", ", GetEqualityComponents());
+
+    public static bool operator ==(ValueObject<T>? left, ValueObject<T> right)
+        => Equals(left, right);
+
+    public static bool operator !=(ValueObject<T> left, ValueObject<T> right)
+        => !Equals(left, right);
+
+    protected abstract IEnumerable<object?> GetEqualityComponents();
+}
